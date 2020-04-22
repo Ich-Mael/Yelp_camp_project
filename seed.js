5dd167a258472f1fd65f8ad39169db41a3ae1b12
Ich-Mael <ismaelzerbo_18@outlook.com>
2020-04-22 15:38:13 +0100

    Campground Style part 1

--- seed.js
+++ seed.js
@@ -5,15 +5,16 @@ const Campground = require("./models/campground");
 data = [
     {name: "Toronto",
      image:"https://media.gettyimages.com/photos/directly-above-shot-of-children-drawing-on-playground-picture-id611465597?s=2048x2048",
-     description:"WOf wof wof wof"
+     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqa phasellus faucibus scelerisque eleifend donec pretium vulputate. Libero id faucibus nisl tincidunt eget nullam."
     },
     {name: "Philadelphia",
      image:"https://media.gettyimages.com/photos/girl-upside-down-on-the-jungle-gym-picture-id1127705002?s=2048x2048",
-     description:"WOf wof wof wof"
+     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolorsque eleifend donec pretium vulputate. Libero id faucibus nisl tincidunt eget nullam."
+
     },
     {name: "houston",
      image:"https://media.gettyimages.com/photos/cropped-view-of-happy-baby-on-swing-at-park-picture-id1155438267?s=2048x2048",
-     description:"WOf wof wof wof"
+     description:"Lorem ipsu. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Libero id faucibus nisl tincidunt eget nullam."
     }
 ];
 
