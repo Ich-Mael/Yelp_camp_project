99f4a44fbc682f0973e5e2e16dab60a6440ffea3
Ich-Mael <ismaelzerbo_18@outlook.com>
2020-05-12 15:25:14 +0100

    destroy route added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -49,25 +49,25 @@ router.post("/", isLoggedIn, (req, res)=>{
     // redirect to campgrounds page
     res.redirect("/campgrounds");
 });
-    // form for a new campground
+// form for a new campground
 
 router.get("/new", isLoggedIn, (req, res)=>{
-        res.render("new");
-    });
+    res.render("new");
+});
 
-    // Show route --- for more description
+// Show route --- for more description
 
-    router.get("/:id", (req, res)=>{
-        let id = req.params.id;
-        Campground.findById(id).populate("comments").exec((err, foundCampground)=>{
-            if(err){
-                console.log(err);
-            }else{
-		console.log(foundCampground);
-                res.render("description",{ campground: foundCampground});
-            }
-        });
+router.get("/:id", (req, res)=>{
+    let id = req.params.id;
+    Campground.findById(id).populate("comments").exec((err, foundCampground)=>{
+        if(err){
+            console.log(err);
+        }else{
+	    console.log(foundCampground);
+            res.render("description",{ campground: foundCampground});
+        }
     });
+});
 
 // Edit Campground Route
 
@@ -76,7 +76,7 @@ router.get("/:id/edit", (req, res)=>{
 	if(err){
 	    res.redirect("/campgrounds");
 	}else{
-    res.render("campgrounds/edit", {campground:foundCampground });
+	    res.render("campgrounds/edit", {campground:foundCampground });
 	}
     }); 
 });
@@ -93,7 +93,17 @@ router.put("/:id", (req, res)=>{
     });
 });
 
+// Destroy campground route
 
+router.delete("/:id", (req, res)=>{
+    Campground.findByIdAndRemove(req.params.id, (err)=>{
+	if(err){
+	    res.redirect("/campgrounds");
+	}else{
+	    res.redirect("/campgrounds");
+	}
+    }); 
+});
 
 //function to check login state
 
