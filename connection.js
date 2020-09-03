cea741e680a3dc34a4eef081612cfdf30573ad33
Ich-Mael <ismaelzerbo18@gmail.com>
2020-09-03 22:13:36 +0100

    Revert "before trying aws"
    This reverts commit e9cf9275d6e1c1e8589b049406c0f0fc2e8d04ed.

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb+srv://ich-mael:aJgQUqWD0PJgZ7fn@cluster0.eak3i.mongodb.net/yelpcampdb?retryWrites=true&w=majority";
+const URI = "mongodb+srv://ich-mael:polipoli1234@cluster0.eak3i.azure.mongodb.net/yelpcampdb?retryWrites=true&w=majority";
 connectDB = async()=>{
 mongoose
      .connect( URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
